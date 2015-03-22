36e618c766a711541057de17949e268c0f943a61
Seth Schwartzman <seth@newrelic.com>
2015-03-21 22:15:05 -0400

    derp

--- alroker.js
+++ alroker.js
@@ -2,7 +2,7 @@ var page = require('webpage').create();
 var my_dashboard = 'your_public_dashboard_id';
 var my_report_name = 'your_report_name';
 
-page.open('https://public.ducksboard.com/' + your_dashboard + '/', function () {
+page.open('https://public.ducksboard.com/' + my_dashboard + '/', function () {
     setTimeout(function() {
     	page.render(my_report_name + '.pdf', {format: 'pdf'});
 		phantom.exit();
